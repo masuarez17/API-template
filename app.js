7f47b840a940ddbca62650699c816e70f2935211
Marcos <suarez0550@gmail.com>
2021-04-19 16:24:36 -0500

    Added register user

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(compression())
 
 app.use(morgan('dev'))
 
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,33 @@
-const usersModel = require('../models/users')
-
-exports.getUser = (request, response) => {
-    response.status(200).json({
-        message: 'ON USERS ROUTES'
-    })
-}
+const database = require('../database')
+const Users = database.models.users
 
 exports.registerUser = (request, response) => {
+    console.log(request)
+    request.query.last_login = new Date()
+    Users.findByPk(request.query.email)
+        .then(result => {
+            if (result === null) {
+                Users.create(request.query)
+                    .then(() => {
+                        response.status(201).json({
+                            message: 'User created successfully'
+                        })
+                    })
+                    .catch(error => {
+                        response.status(500).json(error.errors)
+                    })
+            } else {
+                response.status(403).json({
+                    error: 'User already exist'
+                })
+            }
+        })
+        .catch(error => {
+            response.status(500).json(error.errors)
+        })
+}
+
+exports.getUser = (request, response) => {
     response.status(200).json({
         message: 'ON USERS ROUTES'
     })
