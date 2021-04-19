9587393212f21468a45b49ac9fc2312a27743496
Marcos <suarez0550@gmail.com>
2021-04-19 09:58:22 -0500

    Added logs

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 // Middlewares
-
+app.use(morgan('dev'))
 // Api Routes
 const usersRoutes = require('./routes/users.js')
 
 app.use('/users', usersRoutes)
+
+// Api route not found
+app.use((request, response) => {
+    response.status(404).json({
+        error: 'Route not found'
+    })
+})
 
 module.exports = app
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) => {
-    console.log('On users route')
     response.status(200).json({
         message: 'ON USERS ROUTES'
     })
